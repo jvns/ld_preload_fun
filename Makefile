f1cea044f13430ef4a4b98baabf37ca66c15db81
Julia Evans <julia@jvns.ca>
2014-11-24 15:49:18 -0500

    moar compile

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 all:
 	gcc random_num.c -o random_num
+	gcc test_dlopen.c -o test_dlopen -ldl
 	gcc -shared -fPIC unrandom.c -o unrandom.so
 	gcc -shared -fPIC undlopen.c -o undlopen.so
--- undlopen.c
+++ undlopen.c
@@ -5,7 +5,7 @@
 void* dlopen(const char *file, int mode)
 {
     static void* (*o_dlopen) ( const char *file, int mode )=0;
-    printf( "dlopen was called with file %s\n" , file);
+    printf("dlopen was called with file %s\n" , file);
     o_dlopen = (void*(*)(const char *file, int mode)) dlsym(RTLD_NEXT,
 "dlopen");
     return (*o_dlopen)( file, mode );
